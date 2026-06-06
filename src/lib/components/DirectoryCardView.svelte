48e439a18d8048f5fc8610af82b4f71e4ca01edf
Jack Sloan <hi@jacksloan.dev>
2026-06-06 14:49:57 -0500

    feat: always show description in card view

--- src/lib/components/DirectoryCardView.svelte
+++ src/lib/components/DirectoryCardView.svelte
@@ -117,7 +117,7 @@
 										{/if}
 									</div>
 
-									{#if !compact && business.description}
+									{#if business.description}
 										<p class="text-xs text-muted-foreground">{business.description}</p>
 									{/if}
 
