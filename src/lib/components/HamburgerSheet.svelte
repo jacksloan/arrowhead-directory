016442827c83e6ff80fc45fd3531edc1a4c70181
Jack Sloan <hi@jacksloan.dev>
2026-06-06 21:04:59 -0500

    fix: increase sidenav right padding to pr-12 for full X button clearance

--- src/lib/components/HamburgerSheet.svelte
+++ src/lib/components/HamburgerSheet.svelte
@@ -16,7 +16,7 @@
 		{/snippet}
 	</SheetTrigger>
 	<SheetContent side="left" class="w-64">
-		<nav class="mt-8 flex flex-col gap-2 pr-10">
+		<nav class="mt-8 flex flex-col gap-2 pr-12">
 			<a
 				href="/"
 				onclick={() => (open = false)}
